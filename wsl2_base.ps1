d8d8553118d70b6e1e3e7e4aa22c97c47f9f6cb0
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2021-12-03 00:02:18 -0500

    Add files via upload

---
+++ wsl2_base.ps1
@@ -0,0 +1,7 @@
+﻿dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi
+.\wsl_update_x64.msi /passive
+New-Item -path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate" -name "AU" -Force
+New-ItemProperty -path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -name "AllowMUUpdateService" -value "1" -type "DWORD" -Force
+New-ItemProperty -path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -name "NoAutoUpdate" -value "0" -type "DWORD" -Force
---
+++ wsl2_importdocker.ps1
@@ -0,0 +1,12 @@
+﻿wsl -s ubuntu
+wsl apt update
+wsl apt install `-y docker-ce docker-ce-cli containerd.io
+wsl service docker start
+wsl docker pull centos
+wsl docker run `-t centos bash ls /
+$dockerContainerID=(wsl docker container ls `-a `| grep `-i centos `| awk `'`{print `$1`}`')
+New-Item -path "$env:USERPROFILE\WSL\centos" -ItemType Directory -Force
+cd "$env:USERPROFILE\WSL\centos"
+wsl docker export $dockerContainerID `> "./centos.tar"
+wsl --import centos . .\centos.tar
+#you can now access using "wsl -d centos"
