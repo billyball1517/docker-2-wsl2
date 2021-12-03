fa3c46345f980cdc59588d4b9cad05a5ba8dc091
billyball1517 <54004311+billyball1517@users.noreply.github.com>
2021-12-03 00:10:54 -0500

    Update wsl2_base.ps1

--- wsl2_base.ps1
+++ wsl2_base.ps1
@@ -2,6 +2,7 @@
 dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
 Invoke-WebRequest -Uri https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -OutFile wsl_update_x64.msi
 .\wsl_update_x64.msi /passive
+remove-item .\wsl_update_x64.msi
 New-Item -path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate" -name "AU" -Force
 New-ItemProperty -path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -name "AllowMUUpdateService" -value "1" -type "DWORD" -Force
 New-ItemProperty -path "HKLM:\Software\Policies\Microsoft\Windows\WindowsUpdate\AU" -name "NoAutoUpdate" -value "0" -type "DWORD" -Force
